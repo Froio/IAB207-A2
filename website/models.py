faa48ffa509d45bfe9b3747ff264e38cd3710007
Thomas Froio <thomasfroio@yahoo.com>
2026-04-21 13:52:52 +1000

    Added base for assignment 2.

---
+++ website/models.py
@@ -0,0 +1,20 @@
+from . import db
+from datetime import datetime
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
